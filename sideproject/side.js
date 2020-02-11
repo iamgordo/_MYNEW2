eec4f76b9a27fe8da2385d111814e176ea0834b8
iamgordo <medicnumberone@aim>
2020-02-11 13:23:33 -0500

    three

--- sideproject/side.js
+++ sideproject/side.js
@@ -3,11 +3,13 @@ var canvasContext = undefined;
 
 var fireball = new Image();
 fireball.src = "./img/fireball.png";
+var ball = new newimage(fireball, 100, 200, 75, 20, 1, 1);
 
 
 function start () {
     canvas = document.getElementById("myCanvas");
     canvasContext = canvas.getContext("2d");
+
 
     screenarea =document.getElementById("gameArea");
     screenarea.addEventListener('click', init);
@@ -19,10 +21,14 @@ function init(e){
 document.addEventListener( 'DOMContentLoaded', start);
 // cant do inertia, head swimming
 function update () {
-
+    ball.x -= ball.speedx;
+    ball.y += ball.s
+    peedy;
 }
 function draw(){
-    canvasContext.drawImage(fireball, 200, 200);
+    // canvasContext.drawImage(fireball, 200, 200, 75, 20);
+    canvasContext.drawImage(ball.img, ball.x, ball.y, ball.width, ball.height);
+
 }
 function gameLoop () {
     canvasContext.fillStyle = "#336699";
@@ -30,5 +36,14 @@ function gameLoop () {
     update();
     draw();
     requestAnimationFrame(gameLoop);
+}
+function newimage(img, x, y, width, height, speedx, speedy){
+    this.img = img;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.speedx = speedx;
+    this.speedy = speedy;
 }
 
